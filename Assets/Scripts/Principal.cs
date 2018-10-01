0dff16f7886da612e0f7c66998009bbd9f232cc5
zhuoIC <nicolimonar@gmail.com>
2018-10-01 13:43:20 +0200

    Intento de menu

---
+++ Assets/Scripts/Principal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Principal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Salir()
+    {
+        if (GUI.Button(new Rect(Screen.height/2, Screen.width/2, 20, 20), "YES"))
+        {
+            Application.Quit();
+        }
+        
+    }
+}
